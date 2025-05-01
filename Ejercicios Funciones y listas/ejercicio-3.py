9fdcb2dfd954cb4855bcbcc9a257f76aee74c690
Lucas Rascovsky <163754020+lucky-rascovsky@users.noreply.github.com>
2025-04-30 23:18:55 -0300

    Actualizado ejercicio-3.py
    Correcion de ejercicio.

--- Ejercicios Funciones y listas/ejercicio-3.py
+++ Ejercicios Funciones y listas/ejercicio-3.py
@@ -4,19 +4,20 @@ especificado, validar los números solicitados dentro de ese rango, guardar en u
 lista y retornarla. El programa principal debe invocar a la función y mostrar por
 pantalla el retorno.
 """
-mi_lista = [0] *10
-
 def pedir_numeros ()->list:
     """
     Funcion sin parametros que recibe una lista, solicita al usuario 10 numeros dentro del rango (1-10), valida los numeros dentro del rango y los guarda en una lista y la retorna.
     """
+    mi_lista = [0] *10
     for i in range(len(mi_lista)):
         numero = int(input("ingrese un numero entre (1-10): "))
         mi_lista[i] = numero
-        print(mi_lista[i])
         while numero < 1 or numero > 10:
             numero = int(input("Ingrese un numero dentro del rango: "))
     return mi_lista
 
-
-pedir_numeros()
+lista_retornada = []
+lista_retornada = pedir_numeros()
+print("Numeros:")
+for i in range(len(lista_retornada)):
+    print(lista_retornada[i])
