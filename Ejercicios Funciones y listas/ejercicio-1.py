8954baaac385bd26d950141edbdc2d4ec8d43af1
Lucas Rascovsky <163754020+lucky-rascovsky@users.noreply.github.com>
2025-04-30 22:51:32 -0300

    Actualizado ejercicio-1.py
    Correcion de ejercicio.

--- Ejercicios Funciones y listas/ejercicio-1.py
+++ Ejercicios Funciones y listas/ejercicio-1.py
@@ -3,19 +3,21 @@ Desarrollar una función que pida 10 nombres de manera secuencial, los
 guarde en una lista y la retorne. El programa principal debe invocar a la función y
 mostrar por pantalla el retorno.
 """
-mi_lista = [0] *10
-
 def pedir_nombre ()->list:
     """
     Funcion sin parametros que recibe una lista/array que solicita 10 nombres al usuario de manera secuencial, los guarda en una lista y retorna la misma.
     """
+    mi_lista = [0] *10
+
     for i in range(len(mi_lista)):
         nombre = input("ingrese un nombre: ")
         mi_lista[i] = nombre
-        print("En la posicion", i, "se encuentra ", nombre)
-    for i in range(len(mi_lista)):
-        print(mi_lista[i])
-    print("Fin del programa.")
+    
     return mi_lista
 
-pedir_nombre()
+
+lista_retornada = []
+lista_retornada = pedir_nombre()
+print("Nombres:")
+for i in range(len(lista_retornada)):
+    print(lista_retornada[i])
